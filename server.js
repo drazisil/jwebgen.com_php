2ba7cacf872285254c03f3e70a78b639b1ab35aa
Joe Becher <jwbecher@drazisil.com>
2019-05-15 12:25:53 -0400

    use cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { handleController } = require('./src/index')
 const app = express()
+var cors = require('cors')
 const port = 3000
 
 var options = {
@@ -14,7 +15,9 @@ var options = {
   },
 }
 
-app.all('*', function(req, res, next) {
+app.use(cors())
+
+app.use(function(req, res, next) {
   console.log(`Requested: ${req.url} via ${req.method}`)
   next() // pass control to the next handler
 })
