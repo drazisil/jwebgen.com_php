4ee5037802ebe6e0793c195ad42568a217a4d45e
Joe Becher <jwbecher@drazisil.com>
2019-05-16 14:14:03 -0400

    start of lookup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-function handlePonyLookup(req, res) {
-  console.log('In ponyLookup')
-  return res.status(500).send(`ponyLookup not implemented`)
+const got = require("got")
+
+async function handlePonyLookup(req, res) {
+    console.log('In ponyLookup')
+    const {ponyid} = req.query
+    const result = JSON.parse((await got(`https://get.ponyisland.net?pony=${ponyid}`, {
+      responseType: 'json',
+    })).body)
+    console.dir(result)
+    return result
+
+    // return res.status(500).send(`ponyLookup not implemented`)
+
 }
 
 const handlers = {
   'ponyLookup': handlePonyLookup
 }
 
-function handleController(req, res) {
+async function handleController(req, res) {
   console.dir(req.query)
 
   const { nextAction } = req.query
 
   if (typeof handlers[nextAction] == "function") {
-    return handlers[nextAction](req, res)
+    return await handlers[nextAction](req, res)
   }
   
 res.status(500).send(`nextAction ${nextAction} not implemented`)
