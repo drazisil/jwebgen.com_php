118316c126da00d1419798eca215931d557a81b3
Joe Becher <jwbecher@drazisil.com>
2019-05-16 11:26:03 -0400

    handler and nodemon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
+function handlePonyLookup(req, res) {
+  console.log('In ponyLookup')
+  return res.status(500).send(`ponyLookup not implemented`)
+}
+
+const handlers = {
+  'ponyLookup': handlePonyLookup
+}
+
 function handleController(req, res) {
   console.dir(req.query)
-  res.status(500).send(`nextAction ${req.query['nextAction']} not implemented`)
+
+  const { nextAction } = req.query
+
+  if (typeof handlers[nextAction] == "function") {
+    return handlers[nextAction](req, res)
+  }
+  
+res.status(500).send(`nextAction ${nextAction} not implemented`)
+  
 }
 
 module.exports = { handleController }
