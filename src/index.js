551b38aea39760c8b06e89aedf018a31b06e20b7
Joseph W Becher <jwbecher@drazisil.com>
2019-05-17 11:47:33 -0400

    linting with eslint

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import App from './views/App.vue'
-import Index from './views/Index.vue'
-import Lookup from './views/Lookup.vue'
-import VS2 from 'vue-script2'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./views/App.vue";
+import Index from "./views/Index.vue";
+import Lookup from "./views/Lookup.vue";
+import VS2 from "vue-script2";
 
-Vue.use(VS2)
+Vue.use(VS2);
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 // 2. Define some routes
 // Each route should map to a component. The "component" can
@@ -15,26 +15,26 @@ Vue.use(VueRouter)
 // `Vue.extend()`, or just a component options object.
 // We'll talk about nested routes later.
 const routes = [
-  { path: '/', component: Index },
-  { path: '/lookup', component: Lookup },
-]
+  { path: "/", component: Index },
+  { path: "/lookup", component: Lookup }
+];
 
 // 3. Create the router instance and pass the `routes` option
 // You can pass in additional options here, but let's
 // keep it simple for now.
 const router = new VueRouter({
-  routes, // short for `routes: routes`
-})
+  routes // short for `routes: routes`
+});
 
 // 4. Create and mount the root instance.
 // Make sure to inject the router with the router option to make the
 // whole app router-aware.
-var app = new Vue({
-  el: '#app',
+new Vue({
+  el: "#app",
   components: {
-    App,
+    App
   },
   router,
-  template: '<App/>',
-})
+  template: "<App/>"
+});
 // Now the app has started!
